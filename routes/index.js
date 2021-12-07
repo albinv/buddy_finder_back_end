b1d2c6acfe72c52b69875ec0003fa203b814ff07
Albin Vincent <albvinc.vinc@gmail.com>
2021-12-07 09:47:21 +0530

    Added required bin/www file which was  missing

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,19 @@ var nodemailer = require('nodemailer');
 // email: uol.studybuddyfinder@gmail.com
 // pass:  studybuddyfinder591
 
-// // endpoint to get a list of all the students in the db
-// router.get('/all-students', (req, res, next) => {
-//   // get all records from DB by not providing a search filter
-//   req.collection.find({})
-//     .toArray()
-//     .then(results => res.json(results))
-//     .catch(error => res.send(error));
-// });
+// endpoint to get a list of all the students in the db
+router.get('/all-students', (req, res, next) => {
+  // get all records from DB by not providing a search filter
+  req.collection.find({})
+    .toArray()
+    .then(results => res.json(results))
+    .catch(error => res.send(error));
+});
+
+// endpoint to get a list of all the students in the db
+router.get('/invoke-matching-process', (req, res, next) => {
+  matchingProcess();
+});
 
 // Endpoint to add a new student
 router.post('/add-student', (req, res, next) => {
